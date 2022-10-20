36db71951e70b88d0ba4451febed2080e9fa613e
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2022-10-20 22:06:28 +1100

    recaptcha callback for last commit didn't work - trying this implementation: https://technotrampoline.com/articles/how-to-enable-your-submit-button-based-on-recaptcha-results/

--- js/custom.js
+++ js/custom.js
@@ -2,32 +2,14 @@ $(function(){
     //Area
     AOS.init({});
 
-    //Netlify Recaptcha
-    var recaptcha = document.forms["quantum-cleaning-contactus-form"]["g-recaptcha-response"];
-    // recaptcha.required = true;
-    // recaptcha.oninvalid = function(e) {
-    //     //https://freefrontend.com/css-notifications/
-    // }
-
     var elem = document.querySelector('.cards');
     var flkty = new Flickity( elem, {
     // options
         wrapAround: true
     });
 
-    function enableFormSubmitButton() {
-        const response = grecaptcha.getResponse()
-        if (response.length !== 0) {
-            // `submitBtn` is the id I gave the form submit button
-            document.getElementById('contactUsSubmitBtn').disabled = false
-        }
+    function recaptchaCallback() {
+        const submitButton = document.getElementById("contact-us-submit-btn");
+        submitButton.removeAttribute("disabled");
     }
-    
-    document.addEventListener("DOMContentLoaded", () => {
-        if (typeof grecaptcha === "object") {
-          document.querySelector('.g-recaptcha').dataset.callback = "enableFormSubmitButton";
-        } else {
-          console.log("Unable to add `data-callback` to grecaptcha as it doesn't exist.");
-        }
-    });
 });
